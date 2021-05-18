4e19332e348fcbefbbfc23e44667ae590a3c877b
brunomeloesilva <brunomeloesilva@gmail.com>
2021-05-18 17:43:19 -0400

    Rodando prometheus

--- src/main/java/br/com/zupacademy/brunomeloesilva/share/seguranca/SecurityConfiguration.java
+++ src/main/java/br/com/zupacademy/brunomeloesilva/share/seguranca/SecurityConfiguration.java
@@ -3,22 +3,23 @@ package br.com.zupacademy.brunomeloesilva.share.seguranca;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests(authorizeRequests ->
-                        authorizeRequests
-                                .antMatchers(HttpMethod.GET, "/api/propostas/**").hasAuthority("SCOPE_propostas:read")
-                                .antMatchers(HttpMethod.GET, "/api/cartoes/**").hasAuthority("SCOPE_cartoes:read")
-                                .antMatchers(HttpMethod.POST, "/api/cartoes/**").hasAuthority("SCOPE_cartoes:write")
-                                .antMatchers(HttpMethod.POST, "/api/propostas/**").hasAuthority("SCOPE_propostas:write")
-                                .anyRequest().authenticated()
-                )
-                .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
-    }
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.cors()
+			.and()
+				.authorizeRequests()
+				.antMatchers(HttpMethod.GET, "/actuator/**").permitAll()
+				.anyRequest().authenticated()
+			.and()
+				.csrf().disable()
+				.oauth2ResourceServer()
+				.jwt();
+	}	
 }
